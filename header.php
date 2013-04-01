bef7a3968c52ff8c70866daec132f0ff30118eb7
Ian Cropper <itcropper@gmail.com>
2013-04-01 13:17:58 -0600

    more changes

---
+++ header.php
@@ -0,0 +1,153 @@
+<head>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Julius+Sans+One' rel='stylesheet' type='text/css'>
+
+
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/flippy.js"></script>
+
+
+	<script>
+
+		$('.carousel').carousel();
+
+		var list_items;
+
+		$(document).ready(function(){
+
+			list_items = $("#menu-list").children();
+
+			var pie_peice = 360 / list_items.length;
+
+			//console.log(pie_peice)
+
+			$(list_items).each(function(index){
+				//console.log((index * pie_peice))
+				$(this).css("-webkit-transform", "rotate(" + (index * pie_peice) + "deg)").css("-webkit-transform-origin", "right");
+				$(this).css("-moz-transform", "rotate(" + (index * pie_peice) + "deg)").css("bottom", (index * 22)).css("margin-right", "150px");
+				$(this).css("-ms-transform", "rotate(" + (index * pie_peice) + "deg)").css("bottom", (index * 22)).css("margin-right", "150px");
+
+
+			});
+
+				$(".menu-item").bind('click',scroll);
+				$(".menu-item").bind('click',rotate);
+
+
+				$('.slide-image').each(function(index){
+
+					var height = $(this).height();
+					var width = $(this).width();
+
+					console.log(width + " : " + height)
+
+					$(this).css("margin-right", width).css('top', "50%").css("margin-bottom", height);
+				});
+
+			//setInterval(flipper, 1000);	
+					
+		
+		});
+
+		  // This is a functions that scrolls to #{blah}link
+		function goToByScroll(id){
+		      // Remove "link" from the ID
+		    id = id.replace("link", "");
+
+		    //$('.menu').css("-webkit-transform", "rotate(" + ($(id).index() * 72) - 360 + "deg)").css("bottom", (index * 20));
+
+		      // Scroll
+		    $('html,body').animate({
+		        scrollTop: $("#"+id).offset().top},
+		        'slow');
+		}
+
+
+
+
+		var scroll = function(e) {
+		      // Prevent a page reload when a link is pressed
+		    	e.preventDefault(); 
+		      // Call the scroll function
+		    	goToByScroll($(this).attr("name"));  
+
+		    	
+
+		    };
+
+		var rotate = function(e){
+
+			//console.log($(this).html())
+
+			var index = $(this).index();
+
+			$(this).css("text-decoration", "underline");
+			$(this).siblings().css("text-decoration", "none");
+
+			var degrees = (360 - (index * (360 / list_items.length)));
+			//console.log(degrees);
+
+			$('#menu-list').animate({  borderSpacing: degrees }, {
+			    step: function(now,fx) {
+			    	//console.log(degrees);
+			      $(this).parent().css('-webkit-transform','rotate('+now+'deg)')
+			      $(this).parent().css('-moz-transform','rotate('+now+'deg)'); 
+			      $(this).parent().css('transform','rotate('+now+'deg)');  
+			    },
+			    duration:'slow'
+			},'linear');
+
+		}
+
+		var form_url = "https://docs.google.com/forms/d/19UO6nnSKklytz1cHwmE7JQ-51U1GBC-gn1UgL8gAnlo/formResponse?embedded=true";
+
+		var form_first_name = "entry.998414391=";
+		var form_last_name = "entry.1227180684="
+		var form_attending = "entry.1265819128=";
+		var form_party_number = "entry.1288379601="
+		var form_other_stuff = "6&draftResponse=%5B%5D%0D%0A&pageHistory=0"
+
+		$("#submit").click(function(){
+
+			var one   = form_first_name + $("name=entry.998414391").text();
+			var two    = form_last_name + $("name=entry.1227180684").text();
+			var three   = form_attending + $("name=entry.1265819128").text();
+			var four = form_party_number + $("name=entry.1288379601").text();
+
+
+
+			$.ajax({
+			  type: "POST",
+			  url: form_url,
+			  data: one + two + three + four,
+			}).done(function( msg ) {
+			  alert( "Data Saved: " + msg );
+			});
+
+		});
+
+
+		function flip(id, image){
+
+			//console.log("here")
+
+			$(id).flippy({
+				content: image,
+				direction:"TOP",
+				duration:"750",
+				onStart:function(){
+					console.log("Let's flip");
+				},
+				onFinish:function(){
+					console.log("ok, it's flipped :)");
+				}
+			});
+		}
+
+
+	</script>
+
+</head>
---
+++ index.php
@@ -0,0 +1,198 @@
+	 
+<?php include 'header.php'; ?>
+<body>
+
+	<div class='menu'>
+
+		<ul id='menu-list'>
+			<li class='menu-item current' name='myCarousel'><a href='#' >Home</a></li>
+			<li class='menu-item' name='announcment'><a href='#' >Announcment</a></li>
+			<li class='menu-item' name='rsvp'><a href='#' >RSVP</a></li>
+			<li class='menu-item' name='our-story'><a href='#' >Our Story</a></li>
+			<li class='menu-item' name='pictures'><a href='#' >Pictures</a></li>
+			<li class='menu-item' name='information'><a href='#' >Information</a></li>
+		</ul>
+
+	</div>
+
+	<div id='page-title'><img src='img/title.png' /> </div>
+
+	<div class='announcement' id='announcment'>
+		<h1>announcment</h1>
+	</div>
+
+<!--             *****************             RSVP            ****************                 -->
+
+	<div class='rsvp' id='rsvp'>
+
+		<div class='form-background'>
+		</div>
+		<h1>RSVP</h1>
+		<div class='form-holder'>
+
+			<form action="https://docs.google.com/forms/d/19UO6nnSKklytz1cHwmE7JQ-51U1GBC-gn1UgL8gAnlo/formResponse?embedded=true" method="POST" id="ss-form" target="_self" onsubmit="">
+
+				<label class="ss-q-item-label" for="entry_998414391">
+					<div class="ss-q-title">First Name
+						<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+						
+					</div>
+
+				</label>
+
+				<input type="text" name="entry.998414391" value="" class="ss-q-short" id="entry_998414391" dir="auto" aria-required="true">
+
+				<label class="ss-q-item-label" for="entry_1227180684">
+					<div class="ss-q-title">Last Name
+						<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+						
+					</div>
+					<div  dir="ltr"></div>
+				</label>
+
+				<input type="text" name="entry.1227180684" value="" id="entry_1227180684" dir="auto" aria-required="true">
+
+
+				<label class="ss-q-item-label" for="entry_1693554162">
+					<div class="ss-q-title">Are you attending?
+						<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+						
+					</div>
+				</label>
+
+				<ul class="ss-choices">
+					<li class="ss-choice-item">
+						<label>
+							<input type="radio" name="entry.1265819128" value="Yes" id="group_1265819128_1" aria-label="Yes">
+							<span >Yes</span>
+						</label>
+					</li> 
+					<li class="ss-choice-item">
+						<label>
+							<input type="radio" name="entry.1265819128" value="No" id="group_1265819128_2"  aria-label="No">
+							<span >No</span>
+						</label>
+					</li>
+				</ul>
+
+
+				<label class="ss-q-item-label" for="entry_1288379601">
+					<div class="ss-q-title">How many are in your party?
+						<label for="itemView.getDomIdToLabel()" aria-label="(Required field)"></label>
+						
+					</div>
+					
+				</label>
+
+				<select name="entry.1288379601" id="entry_1288379601" aria-required="true">
+					<option value=""></option>
+					<option value="1">1</option> 
+					<option value="2">2</option> 
+					<option value="3">3</option> 
+					<option value="4">4</option> 
+					<option value="5">5</option> 
+					<option value="6">6</option> 
+					<option value="7">7</option> 
+					<option value="8">8</option>
+				</select>
+
+
+
+				<input type="hidden" name="draftResponse" value="[]">
+				<input type="hidden" name="pageHistory" value="0">
+
+			</br>
+				<input type="submit" name="submit" value="Submit" id="ss-submit">
+
+
+			</form>
+
+		</div>
+
+
+	</div>
+
+	<div class='our_story' id='our-story'>
+		<h1>Our Story</h1>
+	</div>
+
+
+<!--             *****************             Pictures            ****************                 -->
+	<div class='pictures' id='pictures'>
+		<h1>Pictures</h1>
+
+
+		<div id="myCarousel" class="carousel slide">
+
+			<div class="carousel-inner">
+				<div class=" active item">
+					<img class='slide-image' src='img/table_images/1.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/2.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/3.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/4.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/5.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/6.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/7.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/8.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/9.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/10.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/11.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/12.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/13.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/14.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/15.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/16.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/17.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/18.jpg' />
+				</div>
+				<div class="item">
+					<img class='slide-image' src='img/table_images/19.jpg' />
+				</div>																																																
+			</div>
+
+
+			<a class="carousel-control left" href="#myCarousel" data-interval="6000" data-slide="prev">&lsaquo;</a>
+			<a class="carousel-control right" href="#myCarousel" data-interval="6000" data-slide="next">&rsaquo;</a>
+		</div>
+
+	</div>
+
+	<div class='information' id='information'>
+		<h1>Information</h1>
+	</div>
+
+</body>
